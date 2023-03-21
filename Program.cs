eb256ad472cf97cc144149b8ea6021d1602366a3
abdurozikov.kuvonchbek@gmail.com <abdurozikov.kuvonchbek@gmail.com>
2023-03-21 10:35:14 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace problem2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            /* a soni berilgan.  0 dan a gacha bo'lgan
+               sonlaryig'indisini toping  va yig'indini konsolga chiqaring.
+               a sonini ham hisobga oling. */
+            while (true)
+            {
+                Console.Write("Sonni kiriting=> "); int.TryParse(Console.ReadLine(), out int n);
+                int sum = 0;
+                for (int i = 1; i <= n; i++) sum += i;
+                Console.WriteLine("Total sum: " + sum);
+            }
+        }
+    }
+}
